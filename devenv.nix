692e06639ded0a764a2e59115fb71713f0848ee3
Gerard Du Pre <gerardit4@gmail.com>
2026-07-01 09:13:35 +0200

    feat: :sparkles: setup devenv

---
+++ devenv.nix
@@ -0,0 +1,19 @@
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}:
+{
+  # https://devenv.sh/languages/
+  languages.javascript = {
+    enable = true;
+    package = pkgs.nodejs_22;
+    pnpm = {
+      enable = true;
+      install.enable = true;
+    };
+
+  };
+  # See full reference at https://devenv.sh/reference/options/
+}
